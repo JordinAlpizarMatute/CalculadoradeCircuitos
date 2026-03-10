27579bbf1e4c87121ab3d121b6e3b38d53702090
Matux.96 <jordin.alpizar96@gmail.com>
2026-03-09 20:41:32 -0600

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/circuit-calculator/', // Cambia esto por el nombre de tu repo
+  base: '/circuit-calculator/',
 })
